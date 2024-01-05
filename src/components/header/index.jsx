5a40e43f6a0d45adb2813942aff3eb152db70621
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-05 16:42:33 +0600

    hero added

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,36 +8,38 @@ import logo from "../../assets/img/logo.png";
 const Header = () => {
   return (
     <header className="header pt-[30px]">
-      <div className="container">
-        <div className="flex justify-between items-center">
-          <div className="flex justify-start items-center gap-[40px]">
-            <Link to="/" className="logo">
-              <img src={logo} alt="logo" />
-            </Link>
-            <Link to={`/`} className="nav-link text-white">
-              Playrooms
-            </Link>
-            <Link to={`/`} className="nav-link text-white">
-              Tokenomics
-            </Link>
-          </div>
-          <div className="flex justify-end items-center gap-[24px]">
-            <Link to="/" className="text-white nav-link text-xl">
-              <FaDiscord />
-            </Link>
-            <Link to={`/`} className="nav-link text-white">
-              <FaXTwitter />
-            </Link>
-            <Link to={`/`} className="nav-link text-white">
-              <FaTelegramPlane />
-            </Link>
-            <Link
-              to={`/`}
-              className="nav-link text-white grad-btn flex justify-center items-center gap-1 py-[8px] px-[12px]"
-            >
-              <span>Whitepaper</span>
-              <FaArrowRight />
-            </Link>
+      <div className="nav">
+        <div className="container">
+          <div className="flex justify-between items-center">
+            <div className="flex justify-start items-center gap-[40px]">
+              <Link to="/" className="logo">
+                <img src={logo} alt="logo" />
+              </Link>
+              <Link to={`/`} className="nav-link text-white">
+                Playrooms
+              </Link>
+              <Link to={`/`} className="nav-link text-white">
+                Tokenomics
+              </Link>
+            </div>
+            <div className="flex justify-end items-center gap-[24px]">
+              <Link to="/" className="text-white nav-link text-xl">
+                <FaDiscord />
+              </Link>
+              <Link to={`/`} className="nav-link text-white">
+                <FaXTwitter />
+              </Link>
+              <Link to={`/`} className="nav-link text-white">
+                <FaTelegramPlane />
+              </Link>
+              <Link
+                to={`/`}
+                className="nav-link text-white grad-btn flex justify-center items-center gap-1 py-[8px] px-[12px]"
+              >
+                <span>Whitepaper</span>
+                <FaArrowRight />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
