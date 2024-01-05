533b3d6c31a463ecd9269cc24770386e21e6b1d6
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-05 13:06:11 +0600

    tailwind and router initiated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./assets/css/globals.css";
+import "./assets/css/tailwind.config.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render("Hello World!");
+root.render(<RouterProvider router={router} />);
